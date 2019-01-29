cfe7a45f26d7786b9b005a87ada52add32c986de
Bernder <46989407+Bernder@users.noreply.github.com>
2019-01-29 12:37:16 +0100

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,16 @@
+#Crear un script que pregunte por el nombre, el apellido, el teléfono, la ciudad, y la edad. Cada valor ha de ser guardado en una variable diferente.
+print("Introduzca su nombre, por favor")
+nombre = input()
+print("Ahora teclee su apellido")
+apellido = input()
+print("Introduzca número de teléfono")
+telefono= input()
+print("¿En qué ciudad reside usted?")
+ciudad = input()
+print("¿Cuántos años tiene usted?")
+edad = input()
+
+#Imprimir, por pantalla, toda la infomarción coleccionada en una frase que combine la información con coherencia (una oración que no sólo sean los
+#datos recogidos).
+
+print("Muchas gracias, ", nombre, apellido, ", su número de teléfono es ", telefono, ", vive usted en " , ciudad, " y su edad es" , edad)
